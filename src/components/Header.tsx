ae72664fc5b4e4ee19e02c204d3ba9330eac798b
Leejinuk <leejin7900@gmail.com>
2024-07-12 22:19:50 +0900

    Fix: Header부분 로그인관련 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { auth, firestore } from "../firebase";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import "../styles/Header.css";
 import Logo from "../assets/images/logo.png";
 import bars from "../assets/icons/bars.svg";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUser(user);
+        const userDoc = await getDoc(doc(firestore, "users", user.uid));
+        if (userDoc.exists()) {
+          setNickname(userDoc.data().nickname);
+        }
+      } else {
+        setUser(null);
+        setNickname("");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      setNickname("");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
   return (
     <header className="header">
       <div className="header-wrap">
@@ -45,16 +78,29 @@ function Header() {
                 갤러리
               </Link>
             </li>
-            <li>
-              <Link to="/membership" className="nav-list">
-                회원가입
-              </Link>
-            </li>
-            <li>
-              <Link to="/login" className="nav-list">
-                로그인
-              </Link>
-            </li>
+            {user ? (
+              <>
+                <li className="user-info">
+                  <span className="nav-list no-hover">{nickname}</span>
+                  <button onClick={handleLogout} className="logout-btn">
+                    로그아웃
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/membership" className="nav-list">
+                    회원가입
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className="nav-list">
+                    로그인
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
         <button className="toggle-btn" onClick={toggleMenu}>
