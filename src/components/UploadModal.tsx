efe15fb34fff48b98d93b5825c5f9c6bd5270c3f
Leejinuk <leejin7900@gmail.com>
2024-07-16 18:10:18 +0900

    Style: UploadModal 모바일 반응형 작업

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -25,20 +25,6 @@ const customModalStyles: ReactModal.Styles = {
     top: "0",
     left: "0",
   },
-  content: {
-    width: "50%",
-    height: "500px",
-    zIndex: "150",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "10px",
-    boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
-    backgroundColor: "white",
-    justifyContent: "center",
-    overflow: "auto",
-  },
 };
 
 function UploadModal({
