9a87e8c578a81e05cb54085f66c6d9f2eb593eff
Leejinuk <leejin7900@gmail.com>
2024-07-20 17:27:21 +0900

    Style: 디자인 수정

--- src/pages/Impromptu.tsx
+++ src/pages/Impromptu.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import CardIcon from "../assets/icons/card.svg";
 import ListIcon from "../assets/icons/list.svg";
+import UploadIcon from "../assets/icons/upload.svg";
+import SortIcon from "../assets/icons/sort.svg";
 import "../styles/Impromptu.css";
 
 function Impromptu() {
@@ -15,9 +17,40 @@ function Impromptu() {
       <section className="impromptu-wrap">
         <div className="impromptu-header">
           <input type="text" placeholder="벙 이름으로 검색" />
-          <button onClick={handleButtonClick}>
-            <img src={switchIcon} alt="아이콘" />
+          <button>
+            <img src={UploadIcon} alt="업로드 아이콘" />
           </button>
+          <button>
+            <img src={SortIcon} alt="정렬 아이콘" />
+          </button>
+          <button onClick={handleButtonClick}>
+            <img src={switchIcon} alt="스위치 아이콘" />
+          </button>
+        </div>
+        <div className="impromptu-main">
+          <div className="main-section">
+            <div className="main-left">
+              <p>7월 20일 (토)</p>
+            </div>
+            <div className="empty" />
+            <div className="main-right">
+              <h2>벙개 제목</h2>
+              <div className="text-sort">
+                <p>시작시간</p>
+                <p>~</p>
+                <p>끝나는 시간</p>
+              </div>
+              <div className="text-sort">
+                <button>참석</button>
+                <button>보류</button>
+              </div>
+              <div className="text-sort">
+                <p>참석 {1}</p>
+                <p>/</p>
+                <p>보류 {3}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
