e57e1cca382608058f772788709fe27ee03589c4
Leejinuk <leejin7900@gmail.com>
2024-07-20 16:40:50 +0900

    Fix: 벙개최 페이지 수정 및 개발중

--- src/pages/Impromptu.tsx
+++ src/pages/Impromptu.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import CardIcon from "../assets/icons/card.svg";
+import ListIcon from "../assets/icons/list.svg";
 import "../styles/Impromptu.css";
 
 function Impromptu() {
+  const [switchIcon, setSwitchIcon] = useState(CardIcon);
+
+  const handleButtonClick = () => {
+    setSwitchIcon((prevIcon) => (prevIcon === CardIcon ? ListIcon : CardIcon));
+  };
+
   return (
     <>
-      <section className="impromptu-wrap"></section>
+      <section className="impromptu-wrap">
+        <div className="impromptu-header">
+          <input type="text" placeholder="벙 이름으로 검색" />
+          <button onClick={handleButtonClick}>
+            <img src={switchIcon} alt="아이콘" />
+          </button>
+        </div>
+      </section>
     </>
   );
 }
